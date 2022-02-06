f15841d6f2d91cd5c4e00cd5451359a440a568e1
hohserg <hohserg1@yandex.ru>
2022-02-06 03:21:57 +0300

    Added modLanguage and cleanup

--- src/main/scala/hohserg/dimension/layers/Main.scala
+++ src/main/scala/hohserg/dimension/layers/Main.scala
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
-@Mod(modid = "dimension_layers",name = "DimensionLayers",version = "1.0")
+@Mod(modid = "dimension_layers", name = "DimensionLayers", version = "1.0", modLanguage = "scala")
 object Main {
   @EventHandler
   def preInit(e: FMLPreInitializationEvent): Unit = {
