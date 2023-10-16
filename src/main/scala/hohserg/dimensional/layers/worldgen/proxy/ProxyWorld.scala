0d3f4c9e17f9baf4ce44bf34ab04efb46ca014f0
hohserg1 <hohserg1@yandex.ru>
2023-10-16 18:57:54 +0300

    Fix ice/snow and prob make things more consistent

--- src/main/scala/hohserg/dimensional/layers/worldgen/proxy/ProxyWorld.scala
+++ src/main/scala/hohserg/dimensional/layers/worldgen/proxy/ProxyWorld.scala
@@ -1,12 +1,14 @@
 package hohserg.dimensional.layers.worldgen.proxy
 
 import hohserg.dimensional.layers.worldgen.VanillaLayer
+import io.github.opencubicchunks.cubicchunks.api.world.IMinMaxHeight
 import net.minecraft.block.Block
 import net.minecraft.block.state.IBlockState
 import net.minecraft.profiler.Profiler
 import net.minecraft.tileentity.{TileEntity, TileEntityLockableLoot}
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.biome.Biome
 import net.minecraft.world.chunk.{Chunk, IChunkProvider}
 import net.minecraft.world.storage.loot.LootTableManager
 import net.minecraft.world.{World, WorldLens}
@@ -18,7 +20,7 @@ class ProxyWorld(original: World, val layer: VanillaLayer)
     layer.spec.dimensionType.createDimension(),
     new Profiler,
     false
-  ) with FakeCubicWorld {
+  ) with FakeCubicWorld with IMinMaxHeight {
   provider.setWorld(this)
 
   override def createChunkProvider(): IChunkProvider = new ProxyChunkProvider(original, layer)
@@ -26,6 +28,10 @@ class ProxyWorld(original: World, val layer: VanillaLayer)
   chunkProvider = createChunkProvider()
 
   lootTable = new LootTableManager(null)
+
+  override def getMinHeight: Int = layer.startBlockY
+
+  override def getMaxHeight: Int = layer.endBlockY + 1
 
   override def isOutsideBuildHeight(pos: BlockPos): Boolean = {
     pos match {
@@ -43,14 +49,14 @@ class ProxyWorld(original: World, val layer: VanillaLayer)
         tile.setLootTable(null, 0)
       case _ =>
     }
-    super.setBlockState(shiftedPos, newState, flags)
+    original.setBlockState(shiftedPos, newState, flags)
   }
 
-  override def setBlockState(pos: BlockPos, state: IBlockState): Boolean = super.setBlockState(layer.shift(pos), state)
+  override def setBlockState(pos: BlockPos, state: IBlockState): Boolean = original.setBlockState(layer.shift(pos), state)
 
   override def isChunkLoaded(x: Int, z: Int, allowEmpty: Boolean): Boolean = WorldLens.isChunkLoaded(original, x, z, allowEmpty)
 
-  override def getBlockState(pos: BlockPos): IBlockState = super.getBlockState(layer.shift(pos))
+  override def getBlockState(pos: BlockPos): IBlockState = original.getBlockState(layer.shift(pos))
 
   override def getTileEntity(pos: BlockPos): TileEntity = original.getTileEntity(layer.shift(pos))
 
@@ -59,30 +65,50 @@ class ProxyWorld(original: World, val layer: VanillaLayer)
     original.setTileEntity(layer.shift(pos), tileEntityIn)
   }
 
-  override def isAirBlock(pos: BlockPos): Boolean = super.isAirBlock(layer.shift(pos))
+  override def isAirBlock(pos: BlockPos): Boolean = original.isAirBlock(layer.shift(pos))
 
   override def markAndNotifyBlock(pos: BlockPos, chunk: Chunk, iblockstate: IBlockState, newState: IBlockState, flags: Int): Unit =
-    super.markAndNotifyBlock(layer.shift(pos), chunk, iblockstate, newState, flags)
+    original.markAndNotifyBlock(layer.shift(pos), chunk, iblockstate, newState, flags)
 
-  override def destroyBlock(pos: BlockPos, dropBlock: Boolean): Boolean = super.destroyBlock(layer.shift(pos), dropBlock)
+  override def destroyBlock(pos: BlockPos, dropBlock: Boolean): Boolean =
+    original.destroyBlock(layer.shift(pos), dropBlock)
 
   override def notifyBlockUpdate(pos: BlockPos, oldState: IBlockState, newState: IBlockState, flags: Int): Unit =
-    super.notifyBlockUpdate(layer.shift(pos), oldState, newState, flags)
+    original.notifyBlockUpdate(layer.shift(pos), oldState, newState, flags)
 
   override def notifyNeighborsOfStateChange(pos: BlockPos, blockType: Block, updateObservers: Boolean): Unit =
-    super.notifyNeighborsOfStateChange(layer.shift(pos), blockType, updateObservers)
+    original.notifyNeighborsOfStateChange(layer.shift(pos), blockType, updateObservers)
 
-  override def neighborChanged(pos: BlockPos, blockIn: Block, fromPos: BlockPos): Unit = super.neighborChanged(layer.shift(pos), blockIn, fromPos)
+  override def neighborChanged(pos: BlockPos, blockIn: Block, fromPos: BlockPos): Unit =
+    original.neighborChanged(layer.shift(pos), blockIn, fromPos)
 
   override def observedNeighborChanged(pos: BlockPos, changedBlock: Block, changedBlockPos: BlockPos): Unit =
-    super.observedNeighborChanged(layer.shift(pos), changedBlock, changedBlockPos)
+    original.observedNeighborChanged(layer.shift(pos), changedBlock, changedBlockPos)
 
-  override def isBlockNormalCube(pos: BlockPos, _default: Boolean): Boolean = super.isBlockNormalCube(layer.shift(pos), _default)
+  override def isBlockNormalCube(pos: BlockPos, _default: Boolean): Boolean =
+    original.isBlockNormalCube(layer.shift(pos), _default)
 
-  override def isSideSolid(pos: BlockPos, side: EnumFacing, _default: Boolean): Boolean = super.isSideSolid(layer.shift(pos), side, _default)
+  override def isSideSolid(pos: BlockPos, side: EnumFacing, _default: Boolean): Boolean =
+    original.isSideSolid(layer.shift(pos), side, _default)
 
-  override def getBlockLightOpacity(pos: BlockPos): Int = super.getBlockLightOpacity(layer.shift(pos))
+  override def getBlockLightOpacity(pos: BlockPos): Int =
+    original.getBlockLightOpacity(layer.shift(pos))
 
-  override def getHeight(x: Int, z: Int): Int = super.getHeight(x, z) - layer.startBlockY
+  override def getHeight(x: Int, z: Int): Int =
+    original.getHeight(x, z) - layer.startBlockY
 
+  override def getBiome(pos: BlockPos): Biome =
+    original.getBiome(layer.shift(pos))
+
+  override def getBiomeForCoordsBody(pos: BlockPos): Biome =
+    original.getBiomeForCoordsBody(layer.shift(pos))
+
+  override def canSeeSky(pos: BlockPos): Boolean =
+    original.canSeeSky(layer.shift(pos))
+
+  override def canBlockSeeSky(pos: BlockPos): Boolean =
+    original.canBlockSeeSky(layer.shift(pos))
+
+  override def getChunksLowestHorizon(x: Int, z: Int): Int =
+    original.getChunksLowestHorizon(x, z)
 }
