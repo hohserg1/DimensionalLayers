42d03473a9692864b11ccf6f9e2c46a3d557ffcf
hohserg1 <hohserg1@yandex.ru>
2023-11-24 06:08:32 +0300

    sync config from file

--- src/main/scala/hohserg/dimensional/layers/Configuration.java
+++ src/main/scala/hohserg/dimensional/layers/Configuration.java
@@ -1,10 +1,20 @@
 package hohserg.dimensional.layers;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = "dimension_layers")
+@Mod.EventBusSubscriber
 public class Configuration {
     public static String defaultPreset = "";
 
     public static boolean worldTypeByDefault = true;
+
+    @SubscribeEvent
+    public static void onConfigChange(ConfigChangedEvent.OnConfigChangedEvent event) {
+        ConfigManager.sync(Main.modid(), Config.Type.INSTANCE);
+    }
 }
