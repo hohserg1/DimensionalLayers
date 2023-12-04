c3aabf57b06de70d1020a30a3fef875520e84154
hohserg1 <hohserg1@yandex.ru>
2023-12-04 23:57:38 +0300

    fix config modid

--- src/main/scala/hohserg/dimensional/layers/Configuration.java
+++ src/main/scala/hohserg/dimensional/layers/Configuration.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Config(modid = "dimension_layers")
+@Config(modid = "dimensional_layers")
 @Mod.EventBusSubscriber
 public class Configuration {
     public static String defaultPreset = "";
