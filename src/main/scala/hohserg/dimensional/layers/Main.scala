3bc84decec67965327dd6d80d0dec13c3da6cf9f
hohserg1 <hohserg1@yandex.ru>
2023-12-22 12:58:59 +0300

    init gson object at init time
    for avoid lag at first opening world customizatino gui

--- src/main/scala/hohserg/dimensional/layers/Main.scala
+++ src/main/scala/hohserg/dimensional/layers/Main.scala
@@ -1,7 +1,7 @@
 package hohserg.dimensional.layers
 
 import hohserg.dimensional.layers.gui.settings.dimension.GuiFakeCreateWorld
-import hohserg.dimensional.layers.preset.DimensionalLayersPreset
+import hohserg.dimensional.layers.preset.{DimensionalLayersPreset, Serialization}
 import net.minecraft.client.gui.GuiCreateWorld
 import net.minecraftforge.client.event.GuiOpenEvent
 import net.minecraftforge.client.event.GuiScreenEvent.ActionPerformedEvent
@@ -27,6 +27,7 @@ object Main {
   @EventHandler
   def postInit(e: FMLPostInitializationEvent): Unit = {
     DimensionalLayersPreset("")
+    println(Serialization.gson)
   }
 
 
