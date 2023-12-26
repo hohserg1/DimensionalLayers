e96e8066e6e9a155bb18452e67331899345ddbd5
hohserg1 <hohserg1@yandex.ru>
2023-12-26 13:35:33 +0300

    Added CubicWorldTypeLayerSpec
    LayerSpec#height no abstract more, each layer spec impl have it own

--- src/main/scala/hohserg/dimensional/layers/preset/LayerSpec.scala
+++ src/main/scala/hohserg/dimensional/layers/preset/LayerSpec.scala
@@ -1,12 +1,15 @@
 package hohserg.dimensional.layers.preset
 
+import hohserg.dimensional.layers.worldgen.proxy.{BaseWorldServer, ProxyWorld}
+import io.github.opencubicchunks.cubicchunks.api.util.Coords
+import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorldType
 import net.minecraft.block.state.IBlockState
 import net.minecraft.init.Biomes
 import net.minecraft.world.biome.Biome
-import net.minecraft.world.{DimensionType, WorldType}
+import net.minecraft.world.chunk.IChunkProvider
+import net.minecraft.world.{DimensionType, World, WorldServer, WorldType}
 
 sealed trait LayerSpec {
-  def height: Int
 }
 
 case class DimensionLayerSpec(dimensionType: DimensionType,
@@ -14,7 +17,29 @@ case class DimensionLayerSpec(dimensionType: DimensionType,
                               topOffset: Int = 0, bottomOffset: Int = 0,
                               worldType: WorldType = WorldType.DEFAULT, worldTypePreset: String = "") extends LayerSpec {
 
-  override def height: Int = 16 - topOffset - bottomOffset
+  def height: Int = 16 - topOffset - bottomOffset
 }
 
 case class SolidLayerSpec(filler: IBlockState, height: Int, biome: Biome = Biomes.PLAINS) extends LayerSpec
+
+case class CubicWorldTypeLayerSpec(cubicWorldType: WorldType with ICubicWorldType, worldTypePreset: String = "",
+                                   dimensionType1: DimensionType = DimensionType.OVERWORLD,
+                                   seedOverride: Option[Long] = None
+                                  ) extends LayerSpec {
+
+  def rangeCube(original: World): (Int, Int) = {
+    val range1 = cubicWorldType.calculateGenerationHeightRange(
+      new BaseWorldServer(
+        null,
+        ProxyWorld.createLayerWorldInfo(original, seedOverride, cubicWorldType, worldTypePreset),
+        dimensionType1.createDimension(),
+        null
+      ) {
+        override def createChunkProvider(): IChunkProvider = ???
+
+        override def isChunkLoaded(x: Int, z: Int, allowEmpty: Boolean): Boolean = ???
+      }.asInstanceOf[WorldServer]
+    )
+    Coords.blockToCube(range1.getMin) -> Coords.blockToCube(range1.getMax)
+  }
+}
