8c713014bf4ee6da0695b44fa552b09599373219
hohserg1 <hohserg1@yandex.ru>
2023-12-27 12:57:58 +0300

    Added few accessors to BaseDimensionLayer

--- src/main/scala/hohserg/dimensional/layers/worldgen/Layer.scala
+++ src/main/scala/hohserg/dimensional/layers/worldgen/Layer.scala
@@ -1,16 +1,19 @@
 package hohserg.dimensional.layers.worldgen
 
 import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
-import hohserg.dimensional.layers.preset.DimensionLayerSpec
+import com.google.common.collect.ImmutableList
+import hohserg.dimensional.layers.preset.{CubicWorldTypeLayerSpec, DimensionLayerSpec}
 import hohserg.dimensional.layers.worldgen.proxy.{ProxyWorld, ShiftedBlockPos}
 import io.github.opencubicchunks.cubicchunks.api.util.Coords
 import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.EnumCreatureType
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world._
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.chunk.Chunk
 import net.minecraft.world.gen.IChunkGenerator
 
+import java.util
 import java.util.concurrent.TimeUnit
 
 sealed trait Layer {
@@ -20,15 +23,21 @@ sealed trait Layer {
 }
 
 sealed trait BaseDimensionLayer extends Layer {
+  def proxyWorld: ProxyWorld
+
   def realEndCubeY: Int = realStartCubeY + height - 1
 
   def realStartBlockY: Int = Coords.cubeToMinBlock(realStartCubeY)
 
   def realEndBlockY: Int = Coords.cubeToMaxBlock(realEndCubeY)
 
-  def virtualStartBlockY: Int
+  def virtualStartCubeY: Int
 
-  def virtualEndBlockY: Int
+  def virtualEndCubeT: Int
+
+  def virtualStartBlockY: Int = Coords.cubeToMinBlock(virtualStartCubeY)
+
+  def virtualEndBlockY: Int = Coords.cubeToMaxBlock(virtualEndCubeT)
 
   def dimensionType: DimensionType
 
@@ -36,12 +45,23 @@ sealed trait BaseDimensionLayer extends Layer {
 
   def markShifted(pos: BlockPos): ShiftedBlockPos = ShiftedBlockPos.markShifted(pos, this)
 
+  def isInLayer(y: Int): Boolean =
+    virtualStartBlockY <= y && y <= virtualEndBlockY
+
+  def executeInLayer[A](pos: BlockPos, f: ShiftedBlockPos => A, default: A): A = {
+    val p = shift(pos)
+    if (p.isInLayer)
+      f(p)
+    else
+      default
+  }
+
   def getPossibleCreatures(creatureType: EnumCreatureType, localPos: BlockPos): util.List[Biome.SpawnListEntry]
 }
 
 class DimensionLayer(original: World, val spec: DimensionLayerSpec, val realStartCubeY: Int) extends BaseDimensionLayer {
-  val virtualStartBlockY: Int = Coords.cubeToMinBlock(spec.bottomOffset)
-  val virtualEndBlockY: Int = Coords.cubeToMaxBlock(16 - spec.topOffset - 1)
+  val virtualStartCubeY: Int = spec.bottomOffset
+  val virtualEndCubeT: Int = 16 - spec.topOffset - 1
 
   val proxyWorld = ProxyWorld(original, this)
   private val provider: WorldProvider = proxyWorld.provider
