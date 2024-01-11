efe13290bd2287c1640bd1002bc192235cd30c5a
hohserg1 <hohserg1@yandex.ru>
2024-01-11 03:35:56 +0300

    extracted GuiSelectDimension

---
+++ src/main/scala/hohserg/dimensional/layers/gui/GuiSelectDimension.scala
@@ -0,0 +1,38 @@
+package hohserg.dimensional.layers.gui
+
+import com.google.common.cache.LoadingCache
+import hohserg.dimensional.layers.gui.GuiSelectDimension.DrawableDim
+import hohserg.dimensional.layers.gui.IconUtils._
+import net.minecraft.world.DimensionType
+import net.minecraftforge.common.DimensionManager
+
+import scala.collection.JavaConverters._
+
+abstract class GuiSelectDimension(parent: GuiBase,
+                                  linesCache: LoadingCache[Integer, Seq[GuiTileList.GuiTileLine[DrawableDim]]] = GuiSelectDimension.dimLinesByLen)
+  extends GuiBase(parent) with GuiTileList.SelectHandler[DrawableDim] {
+
+  override def initGui(): Unit = {
+    super.initGui()
+    addButton(new GuiClickableButton(width - 100, height - 30, 90, 20, "Cancel")(back))
+    addElement(new GuiTileList(this, x = 10, y = 10, availableWidth = width - 110, height = height - 20, IconUtils.width, linesCache)())
+  }
+}
+
+object GuiSelectDimension {
+
+  final val itemWidth = IconUtils.width
+
+
+  lazy val allDimensions: Seq[DrawableDim] = DimensionManager.getRegisteredDimensions.keySet().asScala.map(DrawableDim).toIndexedSeq
+
+  val dimLinesByLen: LoadingCache[Integer, Seq[GuiTileList.GuiTileLine[DrawableDim]]] = GuiTileList.createLinesCache(allDimensions, itemWidth)
+
+  case class DrawableDim(dimensionType: DimensionType) extends Drawable {
+    override def draw(minX: Int, minY: Int, maxX: Int, maxY: Int): Unit =
+      drawLogo(dimensionType, minX, minY)
+
+    override def tooltip: String =
+      makeDimensionTypeLabel(dimensionType)
+  }
+}
--- src/main/scala/hohserg/dimensional/layers/gui/add/dimension/GuiAddLayer.scala
+++ src/main/scala/hohserg/dimensional/layers/gui/add/dimension/GuiAddLayer.scala
@@ -1,17 +1,11 @@
 package hohserg.dimensional.layers.gui.add.dimension
 
-import hohserg.dimensional.layers.gui.add.dimension.GuiDimensionList.DrawableDim
+import hohserg.dimensional.layers.gui.GuiSelectDimension
+import hohserg.dimensional.layers.gui.GuiSelectDimension.DrawableDim
 import hohserg.dimensional.layers.gui.preset.GuiSetupDimensionalLayersPreset
-import hohserg.dimensional.layers.gui.{GuiBase, GuiClickableButton, GuiTileList}
 import hohserg.dimensional.layers.preset.DimensionLayerSpec
 
-class GuiAddLayer(parent: GuiSetupDimensionalLayersPreset) extends GuiBase(parent) with GuiTileList.SelectHandler[DrawableDim] {
-  override def initGui(): Unit = {
-    super.initGui()
-    addButton(new GuiClickableButton(width - 100, height - 30, 90, 20, "Cancel")(back))
-    addElement(new GuiDimensionList(this, width - 110))
-  }
-
+class GuiAddLayer(parent: GuiSetupDimensionalLayersPreset) extends GuiSelectDimension(parent) {
   override def onSelected(item: DrawableDim): Unit = {
     parent.layersList.add(DimensionLayerSpec(item.dimensionType))
     back()
