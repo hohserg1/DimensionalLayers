95d0f440b3ca96c01e64b3101e4d1694ad7711ee
hohserg1 <hohserg1@yandex.ru>
2024-01-11 03:37:38 +0300

    move highlight draw logic to package object

--- src/main/scala/hohserg/dimensional/layers/gui/package.scala
+++ src/main/scala/hohserg/dimensional/layers/gui/package.scala
@@ -15,5 +15,46 @@ package object gui {
   def makeWorldTypeLabel(worldType: WorldType): String =
     I18n.format("selectWorld.mapType") + " " + I18n.format(worldType.getTranslationKey)
 
+  def drawHighlightHovering(area: DrawableArea)(implicit container: Container): Unit = {
+    drawHighlightHovering(area.x, area.y, area.w, area.h)
+  }
+
+  def drawHighlightHovering(xx: Int, yy: Int, ww: Int, hh: Int): Unit = {
+    drawHighlight(xx, yy, ww, hh, 136, 146, 201)
+  }
+
+  def drawHighlight(xx: Int, yy: Int, ww: Int, hh: Int, red: Int = 255, green: Int = 255, blue: Int = 255): Unit = {
+    GlStateManager.disableTexture2D()
+    val z = -100
+    val tess = Tessellator.getInstance()
+    val buffer = tess.getBuffer
+    buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR)
+
+    buffer.pos(xx - 1, yy, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy - 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx - 1, yy - 1, z).color(red, green, blue, 255).endVertex()
+
+
+    buffer.pos(xx - 1, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx, yy - 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx - 1, yy - 1, z).color(red, green, blue, 255).endVertex()
+
+
+    buffer.pos(xx + ww, yy - 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy - 1, z).color(red, green, blue, 255).endVertex()
+
+
+    buffer.pos(xx - 1, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy + hh + 1, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx + ww + 1, yy + hh, z).color(red, green, blue, 255).endVertex()
+    buffer.pos(xx - 1, yy + hh, z).color(red, green, blue, 255).endVertex()
+
+    tess.draw()
+    GlStateManager.enableTexture2D()
+  }
 
 }
