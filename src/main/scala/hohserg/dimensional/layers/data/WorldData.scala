01e7837f26ae5b2823e712e30c6d5066bf98e891
hohserg1 <hohserg1@yandex.ru>
2024-09-30 22:01:17 +0300

    mapping dim id->layers in WorldData

--- src/main/scala/hohserg/dimensional/layers/data/WorldData.scala
+++ src/main/scala/hohserg/dimensional/layers/data/WorldData.scala
@@ -1,16 +1,20 @@
 package hohserg.dimensional.layers.data
 
 import hohserg.dimensional.layers.CCWorld
-import hohserg.dimensional.layers.data.layer.base.Layer
+import hohserg.dimensional.layers.data.layer.base.{DimensionalLayer, Layer}
 import hohserg.dimensional.layers.preset.DimensionalLayersPreset
-import io.github.opencubicchunks.cubicchunks.api.util.Coords
+import io.github.opencubicchunks.cubicchunks.api.util.{Coords, IntRange}
 import net.minecraft.entity.Entity
 
 
 class WorldData(val original: CCWorld) {
   val preset = DimensionalLayersPreset.fromJson(original.getWorldInfo.getGeneratorOptions)
 
-  val layerAtCubeY: Map[Int, Layer] = preset.toLayerMap(preset.toLayerSeq(original), identity)
+  private val seq: Seq[(IntRange, Layer)] = preset.toLayerSeq(original)
+
+  val layerAtCubeY: Map[Int, Layer] = preset.toLayerMap(seq, identity)
+
+  val dimensionRelatedLayers: Map[Int, Seq[Layer]] = seq.map(_._2).collect { case l: DimensionalLayer => l }.groupBy(_.dimensionType.getId)
 
   def getLayerOf(entity: Entity): Option[Layer] = {
     val y = Coords.blockToCube((entity.posY + 0.5).toInt)
