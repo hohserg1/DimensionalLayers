b4ccd0407b8b2cab66f4a217f5b996405db4ccd5
hohserg1 <hohserg1@yandex.ru>
2024-10-01 13:30:35 +0300

    keep link to used teleporter
    potentialy generate portal at exit pos

---
+++ src/main/scala/hohserg/dimensional/layers/replace/teleport/to/dimension/ILastUsedTeleporter.java
@@ -0,0 +1,7 @@
+package hohserg.dimensional.layers.replace.teleport.to.dimension;
+
+import net.minecraftforge.common.util.ITeleporter;
+
+public interface ILastUsedTeleporter {
+    ITeleporter getLastUsedTeleporter();
+}
---
+++ src/main/scala/hohserg/dimensional/layers/replace/teleport/to/dimension/mixin/CatchEntityTeleporter.java
@@ -0,0 +1,29 @@
+package hohserg.dimensional.layers.replace.teleport.to.dimension.mixin;
+
+import hohserg.dimensional.layers.replace.teleport.to.dimension.ILastUsedTeleporter;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.common.util.ITeleporter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class CatchEntityTeleporter implements ILastUsedTeleporter {
+
+    private ITeleporter lastUsedTeleporter;
+
+    @Inject(
+            method = "changeDimension(ILnet/minecraftforge/common/util/ITeleporter;)Lnet/minecraft/entity/Entity;",
+            at = @At("HEAD")
+    )
+    public void changeDimension(int dimensionIn, ITeleporter teleporter,
+                                CallbackInfoReturnable<Entity> ci) {
+        lastUsedTeleporter = teleporter;
+    }
+
+    @Override
+    public ITeleporter getLastUsedTeleporter() {
+        return lastUsedTeleporter;
+    }
+}
