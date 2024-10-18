cc33137a6f630cc9216c82b6b1b8e4fd092c846a
hohserg1 <hohserg1@yandex.ru>
2024-10-19 01:36:37 +0300

    steps for otg layer settings

--- src/main/scala/hohserg/dimensional/layers/gui/settings/otg/GuiSettingsLayer.scala
+++ src/main/scala/hohserg/dimensional/layers/gui/settings/otg/GuiSettingsLayer.scala
@@ -1,16 +1,21 @@
 package hohserg.dimensional.layers.gui.settings.otg
 
 import hohserg.dimensional.layers.gui.GuiBaseSettings.ValueHolder
-import hohserg.dimensional.layers.gui.GuiTextFieldElement
+import hohserg.dimensional.layers.gui.GuiTileList.SelectHandler
 import hohserg.dimensional.layers.gui.RelativeCoord.{alignLeft, alignTop}
 import hohserg.dimensional.layers.gui.preset.GuiSetupDimensionalLayersPreset
 import hohserg.dimensional.layers.gui.settings.GuiBaseSettingsLayer
+import hohserg.dimensional.layers.gui.settings.solid.GuiBlocksList
+import hohserg.dimensional.layers.gui.{GuiBlockSelector, GuiClickableButton, GuiTextFieldElement}
 import hohserg.dimensional.layers.preset.{LayerSpec, OpenTerrainGeneratorLayerSpec}
+import net.minecraft.block.state.IBlockState
 
 class GuiSettingsLayer(parent: GuiSetupDimensionalLayersPreset, layer: OpenTerrainGeneratorLayerSpec, index: Int)
-  extends GuiBaseSettingsLayer(parent, index) {
+  extends GuiBaseSettingsLayer(parent, index)
+    with SelectHandler[GuiBlocksList.DrawableBlock] {
 
   val seedOverrideH = new ValueHolder[String](layer.seedOverride.map(_.toString).getOrElse(""))
+  val portalMaterialH = new ValueHolder[IBlockState](???)
 
   override def buildLayerSpec(): LayerSpec = layer.copy(seedOverride = toLongSeed(seedOverrideH.get))
 
@@ -19,5 +24,10 @@ class GuiSettingsLayer(parent: GuiSetupDimensionalLayersPreset, layer: OpenTerra
 
     val seedOverrideField = addElement(new GuiTextFieldElement(width - 180, height / 2 - 20 - 20, 170, 20, seedOverrideH, identity))
     addCenteredLabel("seed override:", alignLeft(seedOverrideField.x + seedOverrideField.width / 2), alignTop(seedOverrideField.y - 13), 0xffa0a0a0)
+
+    addButton(new GuiClickableButton(0, 0, 100, 20, "Portal material")(show(new GuiBlockSelector(_, None))))
+
   }
+
+  override def onSelected(item: GuiBlocksList.DrawableBlock): Unit = ???
 }
