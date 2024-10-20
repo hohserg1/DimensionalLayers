dbe1b15eaf4bd3ab45cdf056c2ac0c5f6b9a3148
hohserg1 <hohserg1@yandex.ru>
2024-10-20 06:59:23 +0300

    removed un used mixins CatchEntityTeleporter

--- src/main/scala/hohserg/dimensional/layers/replace/teleport/to/dimension/ILastUsedTeleporter.java
+++
@@ -1,7 +0,0 @@
-package hohserg.dimensional.layers.replace.teleport.to.dimension;
-
-import net.minecraftforge.common.util.ITeleporter;
-
-public interface ILastUsedTeleporter {
-    ITeleporter getLastUsedTeleporter();
-}
