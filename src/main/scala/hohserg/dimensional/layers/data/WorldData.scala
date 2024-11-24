8a00613a1af58730008d4528075bfc1ae75fb6e5
hohserg1 <hohserg1@yandex.ru>
2024-11-25 00:35:41 +0300

    cache min/max Y in WorldData

--- src/main/scala/hohserg/dimensional/layers/data/WorldData.scala
+++ src/main/scala/hohserg/dimensional/layers/data/WorldData.scala
@@ -18,6 +18,11 @@ class WorldData(val original: CCWorld) {
 
   lazy val dimensionRelatedLayers: Map[DimensionId, Seq[DimensionalLayer]] = layers.map(_._2).collect { case l: DimensionalLayer => l }.groupBy(_.dimensionType.getId)
 
+  val minCubeY: Int = layers.minBy(_._1.getMin)._1.getMin
+  val maxCubeY: Int = layers.maxBy(_._1.getMax)._1.getMax
+  val minBlockY = minCubeY << 4
+  val maxBlockY = (maxCubeY << 4) + 15
+
   def getLayerOf(entity: Entity): Option[Layer] = {
     val y = Coords.blockToCube((entity.posY + 0.5).toInt)
     layerAtCubeY.get(y)
