f92224821c855f6966903f98ac1cdfc91d1b0771
hohserg1 <hohserg1@yandex.ru>
2025-10-26 21:36:14 +0300

    extracted sided layer manager

--- src/main/scala/hohserg/dimensional/layers/feature/GrantPotionInLayer.scala
+++ src/main/scala/hohserg/dimensional/layers/feature/GrantPotionInLayer.scala
@@ -1,6 +1,6 @@
 package hohserg.dimensional.layers.feature
 
-import hohserg.dimensional.layers.data.{LayerManagerClient, LayerManagerServer}
+import hohserg.dimensional.layers.data.{LayerManager, LayerManagerClient, LayerManagerServer}
 import hohserg.dimensional.layers.preset.spec.PotionEffectGranting
 import net.minecraft.potion.PotionEffect
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
@@ -13,10 +13,7 @@ object GrantPotionInLayer {
   @SubscribeEvent
   def tick(e: PlayerTickEvent): Unit = {
     val world = e.player.getEntityWorld
-    (if (world.isRemote)
-      LayerManagerClient
-    else
-      LayerManagerServer).getWorldData(world) match {
+    LayerManager.getSided(world).getWorldData(world) match {
       case Some(data) =>
         data.getLayerOf(e.player).filter(l => l.hasPotionEffectGranting).foreach(l => {
           l.spec.additionalFeatures.foreach {
