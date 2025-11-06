d468f9d1214b021a559ede5e831b196295b784a9
hohserg1 <hohserg1@yandex.ru>
2025-11-07 02:18:05 +0300

    reset opengl color before additional feature render

--- src/main/scala/hohserg/dimensional/layers/gui/settings/additional/features/AdditionalFeaturesList.scala
+++ src/main/scala/hohserg/dimensional/layers/gui/settings/additional/features/AdditionalFeaturesList.scala
@@ -5,7 +5,7 @@ import hohserg.dimensional.layers.gui.settings.additional.features.GuiPotionList
 import hohserg.dimensional.layers.gui.{DrawableArea, GuiScrollingListElement, drawWithTexture}
 import hohserg.dimensional.layers.preset.spec.{AdditionalFeature, BlockReplacing, PotionEffectGranting}
 import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.{RenderHelper, Tessellator}
+import net.minecraft.client.renderer.{GlStateManager, RenderHelper, Tessellator}
 import net.minecraft.client.resources.I18n
 import net.minecraft.item.ItemStack
 
@@ -29,6 +29,7 @@ class AdditionalFeaturesList(x: Int, y: Int, w: Int, h: Int, holder: ValueHolder
       val renderItem = Minecraft.getMinecraft.getRenderItem
       features.apply(index) match {
         case BlockReplacing(from, to) =>
+          GlStateManager.color(1, 1, 1, 1)
           RenderHelper.enableGUIStandardItemLighting()
           renderItem.renderItemIntoGUI(new ItemStack(from.getBlock), left + 5, top + 1)
           renderItem.renderItemIntoGUI(new ItemStack(to.getBlock), left + 2 + 18 + 15 + 1, top + 1)
@@ -39,6 +40,7 @@ class AdditionalFeaturesList(x: Int, y: Int, w: Int, h: Int, holder: ValueHolder
           arrowContainer.maxY = top + height
           drawWithTexture(texture, arrowArea.draw(_)(using arrowContainer))
         case PotionEffectGranting(effect, amplifier, playerOnly) =>
+          GlStateManager.color(1, 1, 1, 1)
           val x = left + 10
           DrawablePotion(effect).draw(x, top + 1, x + height, top + 1 + height)
           Minecraft.getMinecraft.fontRenderer.drawStringWithShadow(I18n.format("enchantment.level." + (amplifier + 1)), left + 7 + height + 2, top + 4, 0xff_FF00FF)
