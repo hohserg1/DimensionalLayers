cf7140db5ae01c62b53cec12554e36414a2b0bb9
hohserg1 <hohserg1@yandex.ru>
2025-11-12 19:38:24 +0300

    Create FixFastutilsUpdate.java

---
+++ src/main/scala/hohserg/dimensional/layers/compatibility/cleanroom/FixFastutilsUpdate.java
@@ -0,0 +1,19 @@
+package hohserg.dimensional.layers.compatibility.cleanroom;
+
+import gloomyfolken.hooklib.api.*;
+import io.github.opencubicchunks.cubicchunks.core.server.*;
+import it.unimi.dsi.fastutil.objects.*;
+import net.minecraft.entity.player.*;
+
+@HookContainer
+public class FixFastutilsUpdate {
+
+    @FieldLens
+    public static FieldAccessor<CubeWatcher, ObjectArrayList<EntityPlayerMP>> playersToAdd;
+
+    @Hook
+    @OnMethodCall(value = "rem", shift = Shift.INSTEAD)
+    public static boolean removeScheduledAddPlayer(CubeWatcher self, EntityPlayerMP player) {
+        return playersToAdd.get(self).remove(player);
+    }
+}
